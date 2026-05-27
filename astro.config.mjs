047f1f88c01499d36a9627b7c870955a3ac4098b
thetitanreddy <bobby93982@gmail.com>
2026-05-27 21:33:52 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
