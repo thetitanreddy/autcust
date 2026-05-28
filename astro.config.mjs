6256efb649066025a94767f0a929c8794f9c8efd
thetitanreddy <bobby93982@gmail.com>
2026-05-28 12:29:56 +0530

    chore: configure Vercel security headers, update site domain, and add robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://autcust.com',
   output: 'server',
   adapter: vercel(),
   integrations: [sitemap()]
